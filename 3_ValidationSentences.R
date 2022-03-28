a5958883ae18f75f01eb065f7ca155a3ad400f5d
ChRauh <christian.rauh@gmail.com>
2022-03-28 14:58:44 +0200

    Validation sentences and apps

---
+++ 3_ValidationSentences.R
@@ -0,0 +1,136 @@
+
+
+# Task: Get a set of sentences with Russia references
+# To create manual coding benchmarks for the LSS score
+
+
+# Packages #####
+library(tidyverse)
+library(quanteda)
+library(countrycode)
+library(spacyr)
+library(LSX)
+
+# UNGD corpus
+ungd <- read_rds("./Data/ungd.rds")
+
+
+# Russia references ####
+ungd$russ.ref <- str_count(tolower(ungd$text), "(russia[a-z]*)|(soviet[a-z]*)")
+sum(ungd$russ.ref > 0)
+
+
+# Sentence tokenization ####
+
+spacy_initialize(model = "en_core_web_sm")
+
+# Target data frame  
+sentences <- data.frame()
+
+# Loop over speeches and tokenize
+
+for (i in 1:nrow(ungd)) {
+  
+  # Progress
+  print(paste0(i, "/", nrow(ungd)))
+  
+  # Tokenize current speech
+  current <- spacy_parse(ungd$text[i]) %>% 
+    group_by(sentence_id) %>% 
+    summarise(sentence = paste(token, collapse = " ") %>% 
+                str_trim()) %>% 
+    filter(str_detect(sentence, "[a-z]")) %>% # Drops empty lines and para numbers
+    mutate(doc_id = ungd$doc_id[i],
+           country = ungd$country[i],
+           state = ungd$state[i],
+           year = ungd$year[i])
+  
+  # Append to target
+  sentences <- rbind(sentences, current)
+}
+
+# Keep sentence level data on disk 
+write_rds(sentences, "./Data/ungd_sentences.rds")
+
+
+
+# Apply conflict-cooperation scale ####
+
+# The trained model
+tmod_lss <- read_rds("./Data/lss_model.rds")
+
+# Dfm of sentences
+corp_sent <- corpus(sentences$sentence, docvars = sentences[,c("doc_id", "sentence_id")])
+tok_sent <- tokens(corp_sent,
+                   remove_punct = TRUE, remove_symbols = TRUE, 
+                   remove_numbers = TRUE, remove_url = TRUE) %>% 
+  tokens_remove(stopwords("en"))
+dfm_sent <- tok_sent %>% 
+  dfm() %>% 
+  dfm_remove(pattern = "")
+
+# Predict polarity based on LSS model
+sentences$lss.fit <- predict(tmod_lss, newdata = dfm_sent)
+hist(sentences$lss.fit)
+
+# Examples
+sentences %>% 
+  arrange(lss.fit) %>% 
+  head(10) %>% 
+  select(c(sentence, lss.fit))
+
+sentences %>% 
+  arrange(desc(lss.fit)) %>% 
+  head(10) %>% 
+  select(c(sentence, lss.fit))
+
+
+
+
+# Sample sentences for human coding ####
+
+# Sample
+set.seed(20220325)
+df <- sentences[sample(1:nrow(sentences), 150), ]
+hist(df$lss.fit)
+
+# Examples
+df %>% 
+  arrange(desc(lss.fit)) %>% 
+  head(10) %>% 
+  select(c(sentence, lss.fit))
+
+# Text cleaning
+df$sentence <- df$sentence %>% 
+  str_replace_all("\\s+", " ")
+
+# Split reasonable sample sizes
+write_rds(df[1:50, ], "./Data/Validation/Set1.rds")
+write_rds(df[51:100, ], "./Data/Validation/Set2.rds")
+write_rds(df[101:150, ], "./Data/Validation/Set3.rds")
+
+
+# Sentences referring to Russia somehow ####
+russ <- sentences %>% 
+  filter(str_detect(tolower(sentence), "(russia[a-z]*)|(soviet[a-z]*)"))
+
+russ %>% 
+  arrange(lss.fit) %>% 
+  head(10) %>% 
+  select(c(sentence, lss.fit))
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
---
+++ ValidationApp-Set2/app.R
@@ -0,0 +1,178 @@
+library(shiny)
+library(tidyverse)
+
+
+# Define UI for slider demo application
+ui <- pageWithSidebar(
+  
+  #  Application title
+  headerPanel(" "),
+  
+  sidebarPanel(
+    h6(textOutput("save.results")), # This is intentionally an empty object.
+    h4("Experiment on classifying diplomatic language."),
+    tags$a("Contact: Christian Rauh", 
+           href="http://www.christian-rauh.eu"),
+    h6(""),
+    h6("Details on research purpose and results are available after the coding exercise."),
+    h6(""),
+    h6(textOutput("counter")),
+    h6(""),
+    h4("\nMany thanks for your help!\n"),
+    # Display the page counter text.
+    
+  ),
+  
+  
+  # Show a table summarizing the values entered
+  mainPanel(
+    # Main Action is where most everything is happening in the
+    # object (where the welcome message, survey, and results appear)
+    uiOutput("MainAction"),
+    # This displays the action Button Next.
+    actionButton("Click.Counter", "Next")    
+  )
+)
+
+
+server <- function(input, output) {
+  
+  # Load the set of sentences to be coded
+  Qlist <<- read_rds("Set2.rds")
+  # Qlist <<- Qlist[1:5,] # Shorter Version for testing purposes
+  
+  # Create an empty vector to hold survey results
+  results <<- rep("", nrow(Qlist))
+  # Name each element of the vector based on the
+  # first column of the Qlist (sentence ID)
+  # names(results)  <<- Qlist[,1]
+
+  # Hit counter
+  output$counter <- 
+    renderText({
+      if (!file.exists("counter.Rdata")) counter <- 0
+      if (file.exists("counter.Rdata")) load(file="counter.Rdata")
+      counter <- counter <<- counter + 1
+      
+      save(counter, file="counter.Rdata")     
+      paste0("You are coder ", counter, ".")
+    })
+  
+  # This renderUI function holds the primary actions of the
+  # survey area.
+  output$MainAction <- renderUI( {
+    dynamicUi()
+  })
+  
+  # Dynamic UI is the interface which changes as the survey
+  # progresses.  
+  dynamicUi <- reactive({
+    
+    # Initially it shows a welcome message. 
+    if (input$Click.Counter==0) {
+      return(
+        list(
+          h4(" \nWelcome!"),
+          h5("In the following you'll see 50 randomly selected sentences from international diplomatic speeches."),
+          strong("For every sentence, please choose whether it conveys a more conflictual or a more cooperative message from your point of view."),
+          br(""),
+          p("Just decide spontaneously without thinking too much about possible contexts. There is no right or wrong, it's only your perception that matters."),
+          br("")
+        )
+      )
+    } 
+
+    
+    # Once the next button has been clicked once we see each question
+    # of the survey.
+    if (input$Click.Counter>0 & input$Click.Counter<=nrow(Qlist))  
+      return(
+        list(
+          h5(paste(input$Click.Counter, "/", nrow(Qlist))), # Progress
+          h3(textOutput("question")),
+          radioButtons("survey", "\nIn your view, the message of this sentence is ...",
+                       choices = c("Conflictual", "Somewhat conflictual", "Neutral", "Somewhat cooperative", "Cooperative"),
+                       selected = NA, inline = F)
+        )
+      )
+    
+    # Finally we see results of the survey as well as a
+    # download button.
+    if (input$Click.Counter>nrow(Qlist))
+      return(
+        list(
+          # h4("View aggregate results"),
+          # tableOutput("surveyresults"),
+          h4("Many thanks for your help!"),
+          downloadButton('downloadData', 'Download your results'),
+          br(),
+          h6("This button won't work ...")
+        )
+      )    
+  })
+  
+  # This reactive function is concerned primarily with
+  # saving the results of the survey for this individual.
+  output$save.results <- renderText({
+    # After each click, save the results of the radio buttons.
+    # try is used because there is a brief moment in which
+    # the if condition is true but input$survey = NULL
+    if ((input$Click.Counter>0)&(input$Click.Counter<=nrow(Qlist)))
+      try(results[input$Click.Counter] <<- input$survey)
+
+    # If the user has clicked through all of the survey questions
+    # save the user choices to the question/text list, store the user number (from counter),
+    # and append data to pre-existing data
+    if (input$Click.Counter==nrow(Qlist)+1) {
+      Qlist$human <<- results[1:nrow(Qlist)]
+      Qlist$coder <<- counter
+      
+        if (file.exists("survey.results.rds")){
+          presults <- read_rds(file="survey.results.rds")
+        }
+        if (!file.exists("survey.results.rds")){
+          presults<-NULL
+        }
+        presults <- presults <<- rbind(presults, Qlist)
+        write_rds(presults, file="survey.results.rds")
+    }
+    # Because there has to be a UI object to call this
+    # function I set up render text that displays the content
+    # of this function.
+    ""
+  })
+  
+  # # This function renders the table of results from the
+  # # survey.
+  # output$surveyresults <- renderTable({
+  #   t(summary(presults))
+  # })
+  
+  # This renders the data downloader
+  output$downloadData <- downloadHandler(
+    filename = "IndividualData.csv",
+    content = function(file) {
+      write.csv(Qlist, file, row.names = F)
+    }
+  )
+  
+  # # The option list is a reactive list of elements that
+  # # updates itself when the click counter is advanced.
+  # option.list <- reactive({
+  #   qlist <- Qlist[input$Click.Counter,3:ncol(Qlist)]
+  #   # Remove items from the qlist if the option is empty.
+  #   # Also, convert the option list to matrix. 
+  #   as.matrix(qlist[qlist!=""])
+  # })
+  
+  # This function shows the question text.
+  output$question <- renderText({
+    paste0(
+      Qlist$sentence[input$Click.Counter]
+    )
+  })
+  
+}
+
+
+shinyApp(ui, server)
